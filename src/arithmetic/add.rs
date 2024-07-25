d32c210fbed47ca69b0aafeccf386c38390d4946
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2024-07-26 00:37:45 +0100

    updated subtract

--- src/arithmetic/add.rs
+++ src/arithmetic/add.rs
@@ -3,7 +3,3 @@ pub fn add_numbers(number_one:i32, number_two:i32) -> i32{
     number_one + number_two
 }
 
-
-fn subtract_numbers(number_one:i32, number_two:i32) -> i32{
-    number_one - number_two
-}
